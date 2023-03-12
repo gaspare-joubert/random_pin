c878e07898e50f48fe2cc67b6b24fef642212608
Gaspare Joubert <gasparejoubert@gascosolutions.com>
2023-03-12 19:06:25 +0000

    Add config and .env variables.

--- config/config.php
+++ config/config.php
@@ -4,5 +4,6 @@
  * You can place your custom package configuration in here.
  */
 return [
-
+    'permitted_characters' => env('RANDOM_PIN_PERMITTED_CHARACTERS','1234'),
+    'number_of_pins_to_get' => env('NUMBER_OF_PINS_TO_GET','1'),
 ];
