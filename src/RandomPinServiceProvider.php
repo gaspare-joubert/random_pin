342bbc6f6f6a0fc334f6c0fa1b916d3f2f2998a8
Gaspare Joubert <gasparejoubert@gascosolutions.com>
2023-03-12 13:10:03 +0000

    Update Service Provider to load migrations.

--- src/RandomPinServiceProvider.php
+++ src/RandomPinServiceProvider.php
@@ -16,7 +16,7 @@ class RandomPinServiceProvider extends ServiceProvider
          */
         // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'random_pin');
         // $this->loadViewsFrom(__DIR__.'/../resources/views', 'random_pin');
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
         // $this->loadRoutesFrom(__DIR__.'/routes.php');
 
         if ($this->app->runningInConsole()) {
