4c96676d40d1400021436197a013adae36ac52f1
Gaspare Joubert <gasparejoubert@gascosolutions.com>
2023-03-12 19:21:11 +0000

    Add return type to method.

--- src/RandomPinFacade.php
+++ src/RandomPinFacade.php
@@ -11,7 +11,7 @@ class RandomPinFacade extends Facade
      *
      * @return string
      */
-    protected static function getFacadeAccessor()
+    protected static function getFacadeAccessor(): string
     {
         return 'random_pin';
     }
