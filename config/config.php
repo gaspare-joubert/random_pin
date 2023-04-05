00a48c7be6b06119a4799e1b8d1397b5c62e8d0a
Gaspare Joubert <gasparejoubert@gascosolutions.com>
2023-04-05 21:55:06 +0100

    Add the pin length as a config variable.

--- config/config.php
+++ config/config.php
@@ -5,5 +5,6 @@
  */
 return [
     'permitted_characters' => env('RANDOM_PIN_PERMITTED_CHARACTERS','1234'),
+    'pin_length' => env('RANDOM_PIN_LENGTH','4'),
     'number_of_pins_to_get' => env('NUMBER_OF_PINS_TO_GET','1'),
 ];
